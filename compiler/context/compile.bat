0404b0a1c8a5c5e3ec24fce5c5c1bfc0c46fd923
qeos <qeos@qeos.ru>
2019-06-15 09:57:05 +0300

    first stage for new target
    add new target: Kolibri (still not complete)
    add some include files from Kolibri OS

--- compiler/context/compile.bat
+++ compiler/context/compile.bat
@@ -1,6 +1,31 @@
 ﻿
+@rem @echo ----------------------------------------------------------------
+@rem context.exe context.218.kolibri /c
+@rem fasm context.218.asm
+
 @echo ----------------------------------------------------------------
-context.exe context.218.kolibri /c
-fasm context.218.asm
-@echo ----------------------------------------------------------------
-context.218.exe context.219.kolibri /c
+@context.218.exe context.219.kolibri /c
+@if %errorlevel% NEQ 0 (
+  echo .................................................
+  echo "ERROR TO COMPILE CONTEXT 218 -> 219"
+  exit /b %errorlevel%
+)
+@fasm context.219.asm
+@if %errorlevel% NEQ 0 (
+  echo .................................................
+  echo "ERROR TO COMPILE ASM -> EXE"
+  exit /b %errorlevel%
+)
+
+@context.219.exe example.ctx /k
+@if %errorlevel% NEQ 0 (
+  echo .................................................
+  echo "ERROR TO COMPILE CONTEXT 219 -> KOLIBRI"
+  exit /b %errorlevel%
+)
+@fasm example.asm
+@if %errorlevel% NEQ 0 (
+  echo .................................................
+  echo "ERROR TO COMPILE ASM -> EXE"
+  exit /b %errorlevel%
+)
